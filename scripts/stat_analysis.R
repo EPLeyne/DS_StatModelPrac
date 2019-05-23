a4cfc8333504908cefbb8eaaa5c8e18027db754d
Emmett Leyne <emmettleyne@gmail.com>
2019-05-23 12:16:26 +1000

    Added statistical analysis

--- scripts/stat_analysis.R
+++ scripts/stat_analysis.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(emmeans)
 library(lmerTest)
+library(IDPmisc)
+library(emmeans)
 
 #linear model
 
@@ -18,10 +20,57 @@ mfi <- read_csv('data/munged_data/MulligansFlatInfiltration_tidy.csv')
 mfi$siteId <- factor(mfi$siteId)
 mfi$element <- factor(mfi$element)
 mfi$discPotential <- factor(mfi$discPotential, levels = c('minus4cm','minus1cm','plus1cm'))
+mfi$log_wI <- NaRV.omit(log(mfi$waterInfiltration.ml_min))
+
+# One of the log results is -Inf which prevents lm analysis
+mfi <- mfi %>% mutate_if(is.numeric, list(~na_if(., -Inf)))
 
 
-#These aren't woring for the logs
-mfi_lm1 <- lmer(log(waterInfiltration.ml_min)~avgMC.percent, data = mfi)
+
+# Model water Infiltration rate against MC% --- significant
+mfi_lm1 <- lm(log_wI ~ avgMC.percent, data = mfi)
 anova(mfi_lm1)
-mfi_lm2 <- lmer(log(waterInfiltration.ml_min)~element*siteId + (1|discPotential), mfi)
+summary(mfi_lm1)
+emmeans(mfi_lm1, pairwise~avgMC.percent)
+
+# Model water Infiltration rate against BD --- not significant
+mfi_lm2 <- lm(log_wI ~ avgBD.g_cm3, data = mfi)
 anova(mfi_lm2)
+summary(mfi_lm2)
+
+#Test if correlation between MC and BD --- significant
+mfi_lm9 <- lm(avgMC.percent~avgBD.g_cm3 + siteId, mfi)
+anova(mfi_lm9)
+summary(mfi_lm9)
+
+# Model water Infiltration rate against site ID as a proxy for the diffent soil conditions -- significant
+mfi_lm3 <- lm(log_wI ~ siteId, data = mfi)
+anova(mfi_lm3)
+summary(mfi_lm3)
+
+# Model water Infiltration rate against site ID & soil conditions (additive) --- site ID significant
+mfi_lm4 <- lm(log_wI ~ siteId + avgMC.percent + avgBD.g_cm3, data = mfi)
+anova(mfi_lm4)
+summary(mfi_lm4)
+
+# Model water Infiltration rate against element --- not significant
+mfi_lm6 <- lm(log_wI ~ element, data = mfi)
+anova(mfi_lm6)
+summary(mfi_lm6)
+
+# Model water Infiltration rate against site ID and element --- site ID significant
+mfi_lm5 <- lm(log_wI ~ siteId * element, data = mfi)
+anova(mfi_lm5)
+summary(mfi_lm5)
+
+# Model water Infiltration rate against site ID and element with the disc potential blocking --- site ID significant
+mfi_lm7 <- lmer(log_wI ~ element * siteId + (1|discPotential), mfi)
+anova(mfi_lm7)
+summary(mfi_lm7)
+
+# Model water infilatration against all variables --- site ID significant
+mfi_lm8 <- lmer(log_wI~element*(siteId + avgBD.g_cm3 + avgMC.percent) + (1|discPotential), mfi)
+anova(mfi_lm8)
+summary(mfi_lm8)
+
+
